6eae2eb3d6f9fd74926a6be15bc77da1b26fc0e1
karinaRocio <kariroxio@gmail.com>
2026-06-02 03:26:35 +0000

    subida inicial

---
+++ demoangular/src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('demoangular');
+usuarios = [
+  { id: 1, nombre: 'Juan', correo: 'juan@example.com' },
+  { id: 2, nombre: 'Ana', correo: 'ana@example.com' },
+  { id: 3, nombre: 'Pedro', correo: 'pedro@example.com' },
+  ];
+  pasatiempos = [
+  { id: 1, nombre: 'Juan', lectura: 'leer libros romanticos' },
+  { id: 2, nombre: 'Ana', musica: 'escuchar musica' },
+  { id: 3, nombre: 'Pedro', futball: 'jugar futball con amigos' },
+  ];
+}
